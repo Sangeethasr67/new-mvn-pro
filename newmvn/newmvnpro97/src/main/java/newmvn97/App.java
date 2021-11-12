e35907e4cd7b6ac39a38ac50cc1329a0bdb64c31
Sangeetha S R <sangeethadr078@gmail.com>
2021-11-12 20:51:39 +0530

    new new

---
+++ newmvn/newmvnpro97/src/main/java/newmvn97/App.java
@@ -0,0 +1,13 @@
+package newmvn97;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
